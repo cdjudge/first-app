e7dd9da7954a3131fe1a23e5b5e8434ce22732f4
Chris Judge <chrisjudge@Chriss-Air.attlocal.net>
2024-08-31 00:35:39 -0400

    nit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,16 @@ import {RouterModule} from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [HomeComponent, RouterModule],  template: `
-    <main>
-      <a [routerLink]="['/']">
-        <header class="brand-name">
-          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-        </header>
-      </a>
-      <section class="content">
-        <router-outlet></router-outlet>
-      </section>
-    </main>
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
   `,
   styleUrls: ['./app.component.css'],
 })
